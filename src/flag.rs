fda14dabb6da1e6f77a6f294192c8c8cbefebf80
Shane Poppleton <shane@alphasg.com.au>
2023-07-18 14:22:04 +1000

    Add more countries flag's

--- src/flag.rs
+++ src/flag.rs
@@ -18,8 +18,22 @@ impl Flag {
 impl Flags {
     pub fn new() -> Self {
         let mut flags = HashMap::new();
+        flags.insert(Country::UnitedArabEmirates, Flag(include_bytes!("../assets/flags/ae.svg")));
+        flags.insert(Country::Argentina, Flag(include_bytes!("../assets/flags/ar.svg")));
         flags.insert(Country::Australia, Flag(include_bytes!("../assets/flags/au.svg")));
+        flags.insert(Country::Belgium, Flag(include_bytes!("../assets/flags/be.svg")));
         flags.insert(Country::Brazil, Flag(include_bytes!("../assets/flags/br.svg")));
+        flags.insert(Country::Canada, Flag(include_bytes!("../assets/flags/ca.svg")));
+        flags.insert(Country::Germany, Flag(include_bytes!("../assets/flags/de.svg")));
+        flags.insert(Country::Denmark, Flag(include_bytes!("../assets/flags/dk.svg")));
+        flags.insert(Country::Spain, Flag(include_bytes!("../assets/flags/es.svg")));
+        flags.insert(Country::Finland, Flag(include_bytes!("../assets/flags/fi.svg")));
+        flags.insert(Country::Ireland, Flag(include_bytes!("../assets/flags/ie.svg")));
+        flags.insert(Country::Morocco, Flag(include_bytes!("../assets/flags/ma.svg")));
+        flags.insert(Country::Norway, Flag(include_bytes!("../assets/flags/no.svg")));
+        flags.insert(Country::Philippines, Flag(include_bytes!("../assets/flags/ph.svg")));
+        flags.insert(Country::NewZealand, Flag(include_bytes!("../assets/flags/nz.svg")));
+        flags.insert(Country::Japan, Flag(include_bytes!("../assets/flags/jp.svg")));
         flags.insert(Country::UnitedStates, Flag(include_bytes!("../assets/flags/us.svg")));
 
         Flags(flags)
